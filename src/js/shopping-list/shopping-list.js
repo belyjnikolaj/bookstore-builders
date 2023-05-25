7aa6a2a6d0b1b7ee1c38509d25dc212957108e9f
vitaliyzeykan <112661803+vitaliyzeykan@users.noreply.github.com>
2023-05-25 15:57:09 +0300

    dark style fix

--- src/js/shopping-list/shopping-list.js
+++ src/js/shopping-list/shopping-list.js
@@ -1,8 +1,9 @@
-
 import sprite from '../../images/sprite.svg';
 
 import amazonIcone from '../../images/shopping-list/amazon-icon.png';
 import amazonXIcone from '../../images/shopping-list/amazon-icon@2x.png';
+import amazonIconeDark from '../../images/shopping-list/amazon@1x.png';
+import amazonXIconeDark from '../../images/shopping-list/amazon@2x.png';
 import appleIcone from '../../images/shopping-list/apple-icon.png';
 import appleXIcone from '../../images/shopping-list/apple-icon@2x.png';
 import bookshopIcone from '../../images/shopping-list/bookshop-icon.png';
@@ -11,15 +12,13 @@ import bookshopXIcone from '../../images/shopping-list/bookshop-icon@2x.png';
 const shoppingList = JSON.parse(localStorage.getItem('shoppingList')) || [];
 const shoppingListContainer = document.querySelector('.shopping-list');
 
-
 const emptyListDiv = document.querySelector('.empty-list');
 
 if (shoppingList.length === 0) {
-  emptyListDiv.style.display = 'block';  // Показати empty-list
+  emptyListDiv.style.display = 'block'; // Показати empty-list
 } else {
-  emptyListDiv.style.display = 'none';  // Приховати empty-list
+  emptyListDiv.style.display = 'none'; // Приховати empty-list
 }
-
 
 function removeFromShoppingList(bookId) {
   const updatedList = shoppingList.filter(item => item._id !== bookId);
@@ -34,7 +33,6 @@ deleteBtns.forEach((btn, index) => {
   });
 });
 
-
 function renderShoppingList() {
   shoppingListContainer.innerHTML = '';
 
@@ -46,9 +44,8 @@ function renderShoppingList() {
       description,
       author,
       amazon_product_url,
-      buy_links: [amazon, apple, , , bookshop]
+      buy_links: [amazon, apple, , , bookshop],
     } = book;
-
 
     const bookElMarkup = `
       <li class="js-card card-shopping">
@@ -63,9 +60,13 @@ function renderShoppingList() {
               <ul class="card-shopping__listLinks">
                 <li class="card-shopping__listItem">
                   <a href="${amazon_product_url}" class="card-shopping__link">
-                    <picture>
+                    <picture display="none">
                       <source srcset="${amazonIcone} 1x, ${amazonXIcone} 2x" type="image/png" />
                       <img class="amazon-icon" src="${amazonIcone}" alt="Amazon" />
+                    </picture>
+                    <picture >
+                      <source srcset="${amazonIconeDark} 1x, ${amazonXIconeDark} 2x" type="image/png" />
+                      <img class="amazon-icon" src="${amazonIconeDark}" alt="Amazon" />
                     </picture>
                   </a>
                 </li>
@@ -97,22 +98,16 @@ function renderShoppingList() {
       </li>
     `;
 
-
     shoppingListContainer.insertAdjacentHTML('beforeend', bookElMarkup);
   });
-
-  
-
 }
 
 renderShoppingList();
 
-
-
-// async function fetchBook() { 
-//   const id = '643282b1e85766588626a080'; 
-//   const resp = await axios.get(`https://books-backend.p.goit.global/books/${id}`).then(response => response.data); 
-//    return resp; 
+// async function fetchBook() {
+//   const id = '643282b1e85766588626a080';
+//   const resp = await axios.get(`https://books-backend.p.goit.global/books/${id}`).then(response => response.data);
+//    return resp;
 
 // }
 
@@ -209,4 +204,3 @@ renderShoppingList();
 import '../dark.js';
 import '../support-ukraine.js';
 import '../modal-card.js';
-
